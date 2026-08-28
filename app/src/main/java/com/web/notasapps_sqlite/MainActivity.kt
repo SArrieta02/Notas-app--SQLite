9290fcb677329ee376e87a42b9beddf094dc3067
SArrieta02 <stevenballestero02@gmail.com>
2026-08-28 09:43:13 -0600

    Diseño principal

--- app/src/main/java/com/web/notasapps_sqlite/MainActivity.kt
+++ app/src/main/java/com/web/notasapps_sqlite/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.web.notasapps_sqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.web.notasapps_sqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.FABAgregarNota.setOnClickListener {
+            Toast.makeText(applicationContext, "Has presionado en el FAB", Toast.LENGTH_SHORT).show()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

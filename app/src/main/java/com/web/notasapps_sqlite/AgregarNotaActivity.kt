d53a99f7e6301d118333ac850258ca8f4f420eae
SArrieta02 <stevenballestero02@gmail.com>
2026-08-29 22:45:55 -0600

    Agregar nota UI

--- app/src/main/java/com/web/notasapps_sqlite/AgregarNotaActivity.kt
+++ app/src/main/java/com/web/notasapps_sqlite/AgregarNotaActivity.kt
@@ -1,25 +1,16 @@
 package com.web.notasapps_sqlite
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.web.notasapps_sqlite.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class AgregarNotaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        binding.FABAgregarNota.setOnClickListener {
-            startActivity(Intent(applicationContext, AgregarNotaActivity::class.java))
-        }
+        setContentView(R.layout.activity_agregar_nota)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
